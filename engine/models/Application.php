c6caf921ba0357aee7b6bde36a37d964903434d6
Ferreira9006 <ferreirawow@gmail.com>
2024-08-03 00:43:39 +0100

    Add .env file (WIP)

--- engine/models/Application.php
+++ engine/models/Application.php
@@ -119,49 +119,10 @@ class Application {
     public static function install($host, $username, $password, $database, $title, $header, $acpPassword)
     {
 		// Open db_config.php file
-        $myfile = fopen ("../config.php", "w") or die("Unable to open file!");
+        $myfile = fopen ("../.env", "w") or die("Unable to open file!");
     
 		// Text to insert on the file
         $txt = '<?php
-
-        /**
-         * App Settings
-         * DO NOT TOUCH UNTIL "User Settings"
-         */
-        
-        session_start();
-        
-        //Error handling
-        unset($_SESSION["message"]);
-        $_SESSION["message"] = null;
-        
-        // App main paths
-        define("BASE_PATH", realpath(__DIR__));
-        define("APP_PATH", empty($app_name) ? BASE_PATH ."/website" : BASE_PATH ."/". $app_name);
-        define("APP_TPL_PATH", empty($app_name) ? "website" : "../". $app_name);
-        define("CORE_PATH", BASE_PATH ."/engine");
-        define("MODEL_PATH", CORE_PATH ."/models");
-        define("ERROR_PATH", CORE_PATH ."/errors");
-        define("CONTROLLER_PATH", APP_PATH ."/controllers");
-        define("INCLUDE_PATH", APP_PATH ."/includes");
-        define("VIEW_PATH", APP_PATH ."/views");
-        define("SCRIPT_PATH", APP_TPL_PATH ."/scripts");
-        
-        // App Constants
-        define("APP_NAME", "Flawless");
-        define ("APP_SLOGAN", "Report bugs with ease.");
-        
-        // Models init
-        foreach (glob(MODEL_PATH."/*.php") as $filename)
-        {
-            spl_autoload_register(
-                function($filename)
-                {
-                    include (MODEL_PATH."/".$filename.".php");
-                }
-            );
-        }
-        
         /**
          * User Settings
          * YOU CAN EDIT FROM HERE BELLOW.
--- index.php
+++ index.php
@@ -4,8 +4,15 @@ include "config.php";
 //System Variables
 $installDir = "install/";
 
+require __DIR__ . '/vendor/autoload.php';
+
+Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/')->load();
+
+echo getenv('APP_ENV');
+echo $_ENV['APP_NAME'];
+
 //Page Action
-if (file_exists($installDir)){
+if (file_exists($installDir)) {
 	header("Location: ".$installDir);
 } else {
 	Application::AppInit();
